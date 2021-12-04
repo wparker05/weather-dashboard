90da21c0a8edaa6740da57bcbe46b6024fee0247
Wesley Rogers <wprogers86@gmail.com>
2021-12-04 00:34:24 -0500

    fixed local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,96 +5,98 @@ var week = document.querySelector('#weekWeather');
 var btnList = document.querySelector('#button-list');
 var count = 0;
 
-var uvIndex= function (lat, lon) {
+var uvIndex = function (lat, lon) {
     var uvUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=e19c35c86c9eaa0f15bd8bb58efaa58c`;
 
     fetch(uvUrl)
-        .then(function(response){
+        .then(function (response) {
             return response.json();
         })
-        .then(function(data){
+        .then(function (data) {
             var showIndex = document.createElement('p');
-        
+
             showIndex.textContent = `UV Index: ${data.current.uvi}`;
             now.append(showIndex);
         })
 }
 
-var today  = function(city) {
-    if(!city) location.replace('./index.html');
-var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=e19c35c86c9eaa0f15bd8bb58efaa58c`;
+var today = function (city) {
+    if (!city) location.replace('./index.html');
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=e19c35c86c9eaa0f15bd8bb58efaa58c`;
 
-fetch(url)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-       
-        var cityName = document.createElement('h2');
-        // var img = document.createElement('img');
-        // var icon = data.weather[0].icon;
-        // img.setAttribute('src',`http://openweathermap.org/img/wn/${icon}@2x.png`);
-        cityName.textContent = `${data.name}`;
-        var showTemp = document.createElement('p');
-        showTemp.textContent = `Temp: ${parseInt(data.main.temp)} F`;
-        var showHum = document.createElement('p');
-        showHum.textContent = `Humidity: ${data.main.humidity}%`;
-        var showWind = document.createElement('p');
-        showWind.textContent = `Wind: ${data.wind.speed} MPH`;
-        var lon =data.coord.lon;
-        var lat = data.coord.lat;
-        
-        
-       
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
 
-        now.append(cityName);
-        now.append(showTemp);
-        now.append(showWind);
-        now.append(showHum);
-        uvIndex(lat,lon);
-        now.style.display = "block";
-    
-    })
+            var cityName = document.createElement('h2');
+            // var img = document.createElement('img');
+            // var icon = data.weather[0].icon;
+            // img.setAttribute('src',`http://openweathermap.org/img/wn/${icon}@2x.png`);
+            cityName.textContent = `${data.name}`;
+            var showTemp = document.createElement('p');
+            showTemp.textContent = `Temp: ${parseInt(data.main.temp)} F`;
+            var showHum = document.createElement('p');
+            showHum.textContent = `Humidity: ${data.main.humidity}%`;
+            var showWind = document.createElement('p');
+            showWind.textContent = `Wind: ${data.wind.speed} MPH`;
+            var lon = data.coord.lon;
+            var lat = data.coord.lat;
+
+
+
+
+            now.append(cityName);
+            now.append(showTemp);
+            now.append(showWind);
+            now.append(showHum);
+            uvIndex(lat, lon);
+            now.style.display = "block";
+
+        })
 }
 
-var fiveCast = function(city){
-    if(!city) location.replace('./index.html');
+var fiveCast = function (city) {
+    if (!city) location.replace('./index.html');
     var fiveDayUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=e19c35c86c9eaa0f15bd8bb58efaa58c`;
 
     fetch(fiveDayUrl)
-        .then(function(response){
+        .then(function (response) {
             return response.json();
         })
-        .then(function(data){
-            for(var i = 3; i < data.list.length; i+=8){
-               var box = document.createElement('div');
-               box.setAttribute('class', 'col-2 forecast bg-primary bg-graident');
-               var dayTime = document.createElement('p');
-               dayTime.textContent = data.list[i].dt_txt.slice(0,11);
-               var showTemp = document.createElement('p');
-               showTemp.textContent = `Temp: ${parseInt(data.list[i].main.temp)} F`;
-               var showHum = document.createElement('p');
-               showHum.textContent = `Humidity: ${data.list[i].main.humidity}%`;
-               var showWind = document.createElement('p');
-               showWind.textContent = `Wind: ${data.list[i].wind.speed} MPH`;
+        .then(function (data) {
+            for (var i = 3; i < data.list.length; i += 8) {
+                var box = document.createElement('div');
+                box.setAttribute('class', 'col-2 forecast bg-primary bg-graident');
+                var dayTime = document.createElement('p');
+                dayTime.textContent = data.list[i].dt_txt.slice(0, 11);
+                var showTemp = document.createElement('p');
+                showTemp.textContent = `Temp: ${parseInt(data.list[i].main.temp)} F`;
+                var showHum = document.createElement('p');
+                showHum.textContent = `Humidity: ${data.list[i].main.humidity}%`;
+                var showWind = document.createElement('p');
+                showWind.textContent = `Wind: ${data.list[i].wind.speed} MPH`;
 
-               box.append(dayTime);
-               box.append(showTemp);
-               box.append(showWind);
-               box.append(showHum);
+                box.append(dayTime);
+                box.append(showTemp);
+                box.append(showWind);
+                box.append(showHum);
                 week.append(box);
-                
+
 
             }
-            
+
         })
 }
 
-var btnListClicked = function( id ,input) {
-     var name = document.getElementById(`${id}`);
-    name.addEventListener('click', function(){
+var btnListClicked = function (id, input) {
+    if (!input) location.replace('./index.html');
+    var name = document.getElementById(`${id}`);
+    name.addEventListener('click', function (event) {
+        event.preventDefault();
         week.textContent = '';
-        now.textContent= '';
+        now.textContent = '';
         today(input);
         fiveCast(input);
 
@@ -103,40 +105,47 @@ var btnListClicked = function( id ,input) {
 }
 
 
-    
 
-var buttonList = function(input){
-    var btnStorage = localStorage.getItem('city') || JSON.stringify({buttons:[]});
+
+var buttonList = function (input) {
+    var btnStorage = localStorage.getItem('city') || JSON.stringify({ buttons: [] });
     var prevBtn = JSON.parse(btnStorage);
-    prevBtn.buttons.push(text.value);
+    if (text.value !== '') prevBtn.buttons.push(text.value);
     localStorage.setItem('city', JSON.stringify(prevBtn));
-    var btnCreate=document.createElement('button');
+    var btnCreate = document.createElement('button');
     btnCreate.setAttribute('id', `btn${count}`)
-    btnCreate.setAttribute('class', `btn-count btn-index`);
+    btnCreate.setAttribute('class', `btn btn-primary`);
     btnCreate.textContent = input;
-    
+
     btnList.append(btnCreate);
 
     btnListClicked(`btn${count}`, input);
     count++;
 }
 
+if (btnList.innerHTML === "") {
+    var btnStorage = localStorage.getItem('city') || JSON.stringify({ buttons: [] });
+    var prevBtnList = JSON.parse(localStorage.getItem('city'));
+    if (prevBtnList !== null) {
+        for (var list of prevBtnList.buttons) {
 
-var prevBtnList =JSON.parse(localStorage.getItem('city'));
-console.log(prevBtnList);
+            buttonList(list);
+        }
+    }
+}
 
 
 
 
-btn.addEventListener('click', function(event){
+btn.addEventListener('click', function (event) {
     event.preventDefault();
-    if(!text.value) location.replace('./index.html');
+    if (!text.value) location.replace('./index.html');
     week.textContent = '';
-    now.textContent= '';
+    now.textContent = '';
     var cityInput = text.value
 
     today(cityInput);
     fiveCast(cityInput);
     buttonList(cityInput);
-    text.value= "";
+    text.value = "";
 })
